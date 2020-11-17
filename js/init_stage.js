be7992e9b71de744031b3ac7fc318082a4eb9547
Damian Drewulski (damiannelus) <damiannelus@gmail.com>
2020-11-17 09:28:11 +0100

    finished splitting code into smaller pieces

--- js/init_stage.js
+++ js/init_stage.js
@@ -1,40 +1,40 @@
 function initiateMainRocketSprite() {
-            main_rocket = new Sprite(resources["images/texture_atlas.json"].textures[asset_uris[0]]);
-            main_rocket.scale.set(0.15)
-            main_rocket.anchor.x = 0;
-            main_rocket.anchor.y = 0;
-            main_rocket.vx = 1;
-            main_rocket.vy = 1;
-            main_rocket.shot_delay = 0;
+    main_rocket = new Sprite(resources["images/texture_atlas.json"].textures[asset_uris[0]]);
+    main_rocket.scale.set(0.15)
+    main_rocket.anchor.x = 0;
+    main_rocket.anchor.y = 0;
+    main_rocket.vx = 1;
+    main_rocket.vy = 1;
+    main_rocket.shot_delay = 0;
 
-            // position sprite
-            // main_rocket.x = Math.ceil((Math.random() * (window.innerWidth) / 2 + (window.innerWidth) / 4));
-            // main_rocket.y = Math.ceil((Math.random() * (window.innerHeight) / 4 + 10 * (window.innerHeight) / 16));
-            main_rocket.x = 120;
-            main_rocket.y = Math.ceil((Math.random() * (window.innerHeight) / 4 + 10 * (window.innerHeight) / 16));
+    // position sprite
+    // main_rocket.x = Math.ceil((Math.random() * (window.innerWidth) / 2 + (window.innerWidth) / 4));
+    // main_rocket.y = Math.ceil((Math.random() * (window.innerHeight) / 4 + 10 * (window.innerHeight) / 16));
+    main_rocket.x = 120;
+    main_rocket.y = Math.ceil((Math.random() * (window.innerHeight) / 4 + 10 * (window.innerHeight) / 16));
 
-            //Add the mr to the stage
-            app.stage.addChild(main_rocket);
+    //Add the mr to the stage
+    app.stage.addChild(main_rocket);
 
-        }
+}
 
-        function initiateEnemyRocketSprites() {
-            for (let index = 1; index < asset_uris.length * 10; index++) {
-                enemy_rockets.push(
-                    new Sprite(resources["images/texture_atlas.json"].textures[asset_uris[index % 3 + 1]])
-                )
-                enemy_rockets[index - 1].x = Math.ceil((Math.random() * (window.innerWidth) / 2 + (window.innerWidth) / 4));
-                enemy_rockets[index - 1].y = Math.ceil((Math.random() * (window.innerHeight) / 4 + (window.innerHeight) / 10));
-                // enemy_rockets[index - 1].scale.x = 1
-                // enemy_rockets[index - 1].scale.y = 1
-                enemy_rockets[index - 1].scale.set(0.15);
-                enemy_rockets[index - 1].vx = 1;
-                enemy_rockets[index - 1].vy = 1;
-                enemy_rockets[index - 1].rotation = Math.PI;
-                enemies.addChild(enemy_rockets[index - 1]);
-            }
-            enemies.vx = 1;
-            enemies.vy = 1;
+function initiateEnemyRocketSprites() {
+    for (let index = 1; index < asset_uris.length * 10; index++) {
+        enemy_rockets.push(
+            new Sprite(resources["images/texture_atlas.json"].textures[asset_uris[index % 3 + 1]])
+        )
+        enemy_rockets[index - 1].x = Math.ceil((Math.random() * (window.innerWidth) / 2 + (window.innerWidth) / 4));
+        enemy_rockets[index - 1].y = Math.ceil((Math.random() * (window.innerHeight) / 4 + (window.innerHeight) / 10));
+        // enemy_rockets[index - 1].scale.x = 1
+        // enemy_rockets[index - 1].scale.y = 1
+        enemy_rockets[index - 1].scale.set(0.15);
+        enemy_rockets[index - 1].vx = 1;
+        enemy_rockets[index - 1].vy = 1;
+        enemy_rockets[index - 1].rotation = Math.PI;
+        enemies.addChild(enemy_rockets[index - 1]);
+    }
+    enemies.vx = 1;
+    enemies.vy = 1;
 
-            app.stage.addChild(enemies);
-        }
+    app.stage.addChild(enemies);
+}
---
+++ js/object_interactions.js
@@ -0,0 +1,64 @@
+function shotABomb() {
+    if (main_rocket.shot_delay == 0) {
+        main_rocket.shot_delay = 10;
+        console.log("shoot a bomb at: " + main_rocket.x + " x " + main_rocket.y)
+        let tmp = new Sprite(resources["images/texture_atlas.json"].textures[bomb_uri])
+        tmp.scale.set(0.1);
+        tmp.x = main_rocket.x + (main_rocket.width - tmp.width) / 2;
+        tmp.y = main_rocket.y;
+        tmp.anchor.x = 0;
+        tmp.anchor.y = 0;
+        tmp.vx = 1;
+        tmp.vy = 1;
+        bombs.push(tmp);
+        app.stage.addChild(tmp);
+    }
+}
+
+function moveBombs() {
+    bombs.forEach(element => {
+        // element.x -= element.vx;
+        element.y -= element.vy;
+    });
+}
+
+function destroyBombsOutsideStage() {
+    if (bombs.length > 0) {
+        for (let index = 0; index < bombs.length; index++) {
+            if (bombs[index].y < 0) {
+                app.stage.removeChild(bombs[index]);
+                bombs.shift();
+            }
+
+        }
+    }
+}
+
+
+function rotateMainRocket(direction) {
+    main_rocket.rotation = direction == "cw" ? main_rocket.rotation + (Math.PI / 20) : main_rocket.rotation - (Math.PI / 20)
+}
+
+function moveEnemies() {
+    let steering_wheel = Math.random();
+    console.log('steering_wheel :>> ', steering_wheel);
+    let xp = steering_wheel > 0.5 ? enemies.x + enemies.vx : enemies.x - enemies.vx;
+    // xp = enemies.xp < 0 ? 0 : enemies.x
+    let yp = (Math.ceil(steering_wheel * 100) % 2) == 0 ? enemies.y + enemies.vy : enemies.y - enemies.vy;
+    // yp = enemies.yp < 0 ? 0 : enemies.y
+    console.log('xp :>> ', xp);
+    console.log('yp :>> ', yp);
+    enemies.position.set(xp, yp);
+}
+
+function checkEnemiesHits() {
+    bombs.forEach(bomb => {
+        enemy_rockets.forEach((er, index) => {
+            if (b.hit(bomb, er)) {
+                enemies.removeChild(er);
+                enemy_rockets.splice(index, 1)
+                console.log("rocket to be destroyed: " + index);
+            }
+        });
+    });
+}
